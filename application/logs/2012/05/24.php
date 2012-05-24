0ea49dc3bd54618851e561e8c45c51886778474f
Richard Thomas <rhomas@gmail.com>
2012-05-24 23:09:04 +0100

    Insult setup

---
+++ application/logs/2012/05/24.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-05-24 17:06:08 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL insults was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+2012-05-24 17:06:08 --- STRACE: HTTP_Exception_404 [ 404 ]: The requested URL insults was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 87 ]
+--
+#0 C:\xampp\htdocs\insults\system\classes\kohana\request\client.php(64): Kohana_Request_Client_Internal->execute_request(Object(Request))
+#1 C:\xampp\htdocs\insults\system\classes\kohana\request.php(1138): Kohana_Request_Client->execute(Object(Request))
+#2 C:\xampp\htdocs\insults\index.php(109): Kohana_Request->execute()
+#3 {main}
